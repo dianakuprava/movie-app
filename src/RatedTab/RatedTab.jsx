7036146c48248aed439214f06189483e9d5572a9
dianakuprava <dianakuprava1@gmail.com>
2025-06-01 22:41:18 +0300

    Update RatedTab.jsx

--- src/RatedTab/RatedTab.jsx
+++ src/RatedTab/RatedTab.jsx
@@ -25,7 +25,6 @@ export default function RatedTab({ sessionId, ratingUpdated }) {
           return;
         }
 
-        // Загрузка фильмов с API
         const data = await fetchRatedMovies(sessionId, currentPage);
         console.log("Fetched movies:", data.results);
 
