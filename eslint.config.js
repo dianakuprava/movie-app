7b6a58cb46b3fd4b3337b312306a8ce67325730e
dianakuprava <dianakuprava1@gmail.com>
2025-06-01 21:09:51 +0300

    fix-errors

---
+++ eslint.config.js
@@ -0,0 +1,50 @@
+// eslint.config.js
+import js from '@eslint/js';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js', '**/*.jsx'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        fetch: 'readonly',
+        localStorage: 'readonly',
+        document: 'readonly',
+        window: 'readonly',
+        console: 'readonly',
+      },
+    },
+    plugins: {
+      react,
+      prettier,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/jsx-uses-vars': 'warn',
+      'no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+];
